194d7a5d3734b79100d243899f466370140f5e9c
EuricoTurzi <riicodt@gmail.com>
2024-10-17 12:18:22 -0300

    Arrumando postgree

--- ps_voucher/settings.py
+++ ps_voucher/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'ps_voucher.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'rrtBHXaiqJjaifySbWWbnezrFxMnSbFM',
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432',
     }
 }
 
